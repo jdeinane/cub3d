6e44d7880958b663206b42b4095fc6cc98d825e5
Jennifer UBALDO <jubaldo@bess-f1r3s2.clusters.42paris.fr>
2024-05-19 16:30:06 +0200

    raycasting struct

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 17:17:11 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/18 18:32:38 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/19 16:29:43 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,23 @@ typedef struct s_keys {
 	bool	left;
 	bool	right;
 }	t_keys;
+
+typedef struct s_ray {
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	int		map_x;
+	int		map_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	perp_wall_dist;
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side;
+}	t_ray;
 
 typedef struct s_cub3d {
 	void		*mlx;
