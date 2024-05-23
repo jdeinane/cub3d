d759360b846cc04063caf4cf69a540db45f66bb8
Jennifer UBALDO <jubaldo@bess-f1r3s6.clusters.42paris.fr>
2024-05-23 15:14:29 +0200

    init_ray

--- raycasting/init_ray.c
+++ raycasting/init_ray.c
@@ -6,7 +6,7 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 16:31:04 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/19 16:31:42 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/23 14:43:44 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	init_ray(t_cub3d *game, t_ray *ray, int x)
 {
-	ray->camera_x = 2 * x 
+	ray->camera_x = 2 * x / (double)game->win_width - 1;
+	ray->ray_dir_x = game->player.dir_x + game->player.plane_x * ray->camera_x;
+	ray->ray_dir_y = game->player.dir_y + game->player.plane_y * ray->camera_x;
+	ray->map_x = (int)game->player.pos_x;
+	ray->map_y = (int)game->player.pos_y;
+	ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
+	ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+	ray->hit = 0;
 }
