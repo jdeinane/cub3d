bfc6932bba2f0049d338d0add943915a74f6a72e
Jennifer UBALDO <jubaldo@made-f0Ar11s1.clusters.42paris.fr>
2024-05-24 20:44:37 +0200

    init rays ameliore

--- raycasting/init_ray.c
+++ raycasting/init_ray.c
@@ -6,11 +6,39 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 16:31:04 by jubaldo           #+#    #+#             */
-/*   Updated: 2024/05/23 16:37:29 by jubaldo          ###   ########.fr       */
+/*   Updated: 2024/05/24 20:44:12 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+static void	init_sides(t_cub3d *game, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (game->player.pos_x - \
+							ray->map_x * ray->delta_dist_x);
+	}
+	else
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (ray->map_x + 1.0 - game->player.pos_x * \
+							ray->delta_dist_x);
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (game->player.pos_y - \
+							ray->map_y * ray->delta_dist_y);
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - game->player.pos_x * \
+							ray->delta_dist_y);
+	}
+}
 
 void	init_ray(t_cub3d *game, t_ray *ray, int x)
 {
@@ -22,4 +50,5 @@ void	init_ray(t_cub3d *game, t_ray *ray, int x)
 	ray->delta_dist_x = fabs(1 / ray->ray_dir_x);
 	ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
 	ray->hit = 0;
+	init_sides(&game, &ray);
 }
